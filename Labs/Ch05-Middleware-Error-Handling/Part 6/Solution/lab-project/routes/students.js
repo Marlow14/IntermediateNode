069e77fe9a60c753d3c2b103318cbb739f7af8bc
Judy Lipinski <judy@karmoxie.com>
2018-01-04 16:36:49 -0500

    added ch03 and ch05

---
+++ Labs/Ch05-Middleware-Error-Handling/Part 6/Solution/lab-project/routes/students.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+ // res.send('Students will go in here');
+    res.render('students', { 
+     title: 'Students' ,
+ //    students: ['Uma','Tucker','Seema','Saroj','Rathna','Ella','Paul']
+    });
+});
+
+module.exports = router;
