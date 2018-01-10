a30396d76546d01829cd2e3aeb6e54053a2d7295
Judy Lipinski <judy@karmoxie.com>
2018-01-10 12:56:44 -0500

    updated comments

--- Labs/Ch04-DatabaseAccess/Part4-3/Solution/lab-project/custom-errors.js
+++
@@ -1,6 +0,0 @@
-const createError = require("create-error");
-
-module.exports = {
-	AuthenticationError: createError("AuthenticationError", {isCustomError: true, status: 401}),
-	ValidationError: createError("ValidationError", {isCustomError: true, status: 422})
-};
