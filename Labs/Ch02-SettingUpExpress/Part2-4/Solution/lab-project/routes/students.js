dabea12fa4000657c978af788ec7a62a35077a91
Judy Lipinski <judy@karmoxie.com>
2018-01-10 02:41:31 -0500

    updated comment

--- Labs/Ch02-SettingUpExpress/Part2-4/Solution/lab-project/routes/students.js
+++ Labs/Ch02-SettingUpExpress/Part2-4/Solution/lab-project/routes/students.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET student listing. */
 router.get('/', function(req, res, next) {
- // res.render('Students will go in here');
  res.render('students', { title: 'Students' });
 });
 
 module.exports = router;
+
