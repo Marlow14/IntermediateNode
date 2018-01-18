ee114e9d1fd7d34a48f01cf52d0f345876f9fc91
Judy Lipinski <judy@karmoxie.com>
2018-01-18 13:20:49 -0500

    hash change

--- Labs/Ch05-Session/Part5-3/Solution/lab-project/migrations/20171115231939_add_hash_column_users.js
+++ Labs/Ch05-Session/Part5-3/Solution/lab-project/migrations/20171115231939_add_hash_column_users.js
@@ -11,6 +11,10 @@ exports.up = function(knex, Promise) {
     // .catch((err) => {
     //     console.log(err);
     // });
+  // rather you'll want to just create the hash column, make it allow NULL values, then run a query afterwards that goes through each row, fetches its password value, hashes it, and stores the result back in the hash column
+
+
+
 };
 
 exports.down = function(knex, Promise) {
