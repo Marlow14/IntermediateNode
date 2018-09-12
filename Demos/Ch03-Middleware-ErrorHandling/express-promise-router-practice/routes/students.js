12f7be4a9857c7a53cb0016ea33ca6d7fe88d709
Judy Lipinski <judy@karmoxie.com>
2018-09-12 13:41:20 -0400

    added students back in

--- Demos/Ch03-Middleware-ErrorHandling/express-promise-router-practice/routes/students.js
+++ Demos/Ch03-Middleware-ErrorHandling/express-promise-router-practice/routes/students.js
@@ -1,6 +1,25 @@
 'use strict';
 
 const Promise = require("bluebird");
+const moment = require("moment");
+
+let students = [{
+	nameFirst: "Devin",
+	nameLast: "Durgan",
+	email: "Devin.Durgan@gmail.com",
+	hireDate: moment("01/19/2015", "MM/DD/YYYY")
+}, {
+	nameFirst: "Cristal",
+	nameLast: "Adams",
+	email: "Cristal.Adams@live.com",
+	hireDate: moment("07/29/2016", "MM/DD/YYYY")
+}, {
+	nameFirst: "Nettie",
+	nameLast: "McGlynn",
+	email: "Nettie.McGlynn@gmail.com",
+	hireDate: moment("08/29/2015", "MM/DD/YYYY")
+}];
+
 
 module.exports = function({db}) {
 	let router = require("express-promise-router")();
