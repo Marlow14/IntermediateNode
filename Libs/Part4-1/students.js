631e8332af6dee18f37c704d293d06513a7bf787
Judy Lipinski <judy@karmoxie.com>
2018-09-14 09:57:49 -0400

    updated readme to use lib students.js

--- Libs/Part4-1/students.js
+++ Libs/Part4-1/students.js
@@ -18,31 +18,6 @@ module.exports = function({db}) {
 		});
 	});
 
-	router.post("/generate", (req, res) => {
-		let skillLevel = [1, 2, 3, 4, 5, 6];
-		let domains = ['@gmail.com', '@yahoo.com', '@live.com', '@aol.com'];
-
-		return Promise.try(() => {
-			return range.range(0, 10);
-		}).map((i) => {
-			let nameFirst = faker.name.firstName();
-			let nameLast = faker.name.lastName();
-			let email = `${nameFirst}.${nameLast}${pickItem(domains)}`;
-
-			return db("students").insert({
-				nameFirst: nameFirst,
-				nameLast: nameLast,
-				email: email,
-				hireDate: faker.date.between('2015-01-01', '2016-12-31'),
-				htmlSkill: pickItem(skillLevel),
-				cssSkill: pickItem(skillLevel),
-				jsSkill: pickItem(skillLevel),
-			});
-		}).then(() => {
-			res.redirect("/");
-		});
-	});
-
 
 
 	return router;
